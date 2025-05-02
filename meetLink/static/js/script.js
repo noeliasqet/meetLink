a43c777ef02781aef670898d6c6cf94a759437e9
msuafue034 <xmsuafue034@ieshnosmachado.org>
2025-05-02 18:22:48 +0200

    02/05 - Cambios en navbar collapse (sin terminar)

--- meetLink/static/js/script.js
+++ meetLink/static/js/script.js
@@ -1,31 +1,4 @@
-function toggleMenu() {
-    const navbar = document.getElementById('navbar');
-    const toggleButton = document.getElementById('toggle-button');
-    navbar.classList.toggle('show');
-    toggleButton.classList.toggle('open');
+function toggleSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.toggle('collapsed');
 }
-
-function handleResize() {
-    const navbar = document.getElementById('navbar');
-    const toggleButton = document.getElementById('toggle-button');
-
-    if (window.innerWidth >= 768) {
-        navbar.classList.add('show');
-    } else if (!toggleButton.classList.contains('open')) {
-        navbar.classList.remove('show');
-    }
-}
-
-window.addEventListener('resize', handleResize);
-window.addEventListener('DOMContentLoaded', handleResize);
-
-document.addEventListener('click', function(e) {
-    const navbar = document.getElementById('navbar');
-    const toggleButton = document.getElementById('toggle-button');
-
-    // Cierra el navbar también si se hace clic fuera
-    if (!navbar.contains(e.target) && !toggleButton.contains(e.target)) {
-        navbar.classList.remove('show');
-        toggleButton.classList.remove('open');
-    }
-});
