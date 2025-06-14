dc651f57e444786fcf81bed9805fc137b30c9d40
msuafue034 <xmsuafue034@ieshnosmachado.org>
2025-06-14 14:59:05 +0200

    Cambios para despliegue

--- proyectoFinal/settings/base.py
+++ proyectoFinal/settings/base.py
@@ -42,9 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'meetLink',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
